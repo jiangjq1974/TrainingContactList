739853de5fad6bb4c1c49baf30e8dd5545a9fd43
jjq <jjq>
2016-07-31 10:33:04 +0800

    Done commit

--- app/src/main/java/com/example/lsx/trainingcontactlist/MainActivity.java
+++ app/src/main/java/com/example/lsx/trainingcontactlist/MainActivity.java
@@ -7,33 +7,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView mContactListView;
-    private List<String> mContactList;
+    private List<String> mContactList = new ArrayList<String>();
     private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContactListView= (ListView) findViewById(R.id.activity_main_contact_list_view);
-        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mContactList);
+        mContactListView = (ListView) findViewById(R.id.activity_main_contact_list_view);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mContactList);
         mContactListView.setAdapter(adapter);
         readContactInfo();
     }
 
     private void readContactInfo() {
-        Cursor cursor=null;
-        cursor=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                null,null,null,null);
-        while (cursor.moveToNext())
-        {
-            String name=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, null);
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            mContactList.add(name+"\n"+number);
+            mContactList.add(name + "\n" + number);
         }
 
     }
